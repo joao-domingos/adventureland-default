93185552b8b8e88c426c9a0cdef6e99a1db3ca97
Joao Domingos <joaodomingos@alunos.utfpr.edu.br>
2023-04-08 22:10:42 -0300

    idk

--- characters/idkhtcmerch.5431614983634944.js
+++
@@ -1,6 +0,0 @@
-setInterval(function(){
-
-if(!is_on_cooldown("fishing")
-	&& character.mp>=G.skills.fishing.mp)
-	use_skill("fishing");
-},1000/4);
--- codes/padrao.1.js
+++
@@ -1,8 +0,0 @@
-//load everything inside setInterval function
-
-setInterval(function(){
-	load(padrao.1.js);
-
-	
-
-},1000/4);
