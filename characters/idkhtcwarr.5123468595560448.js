8ae758970c8ebd746b15c3d0f87cf2a4dd3bbf67
Joao Domingos <joaodomingos@alunos.utfpr.edu.br>
2023-04-09 04:58:08 -0300

    new shit

--- characters/idkhtcwarr.5123468595560448.js
+++ characters/idkhtcwarr.5123468595560448.js
@@ -1,49 +1,51 @@
-// Hey there!
-// This is CODE, lets you control your character with code.
-// If you don't know how to code, don't worry, It's easy.
-// Just set attack_mode to true and ENGAGE!
+// autorerun
 
-var attack_mode=true
+load_code(1);
+load_code("mainfunctions");
+
+let attack_mode = true;
+let assist_mode = true;
+let skills_mode = true;
+
 
 setInterval(function(){
-
-	
-	load_code("killToLvUp");
-	load_code("codeCost");
-	use_hp_or_mp();
-	loot();
-
-	if(!attack_mode || character.rip || is_moving(character)) return;
-
-	var target=get_targeted_monster();
-	if(!target)
-	{
-		target=get_nearest_monster({id:"crabx"});
-		if(target) change_target(target);
-		else
-		{
-			set_message("No Monsters");
-			return;
-		}
-	}
-	
-	if(!is_in_range(target))
-	{
-		move(
-			character.x+(target.x-character.x)/2,
-			character.y+(target.y-character.y)/2
-			);
-		// Walk half the distance
-	}
-	else if(can_attack(target))
-	{
-		set_message("Attacking");
-		attack(target);
-	}
+	heal_hp_or_mp();
+	funcLoop();
+    if(character.rip) handleDeath();
+	if(!attack_mode || character.rip) return;
+    followBot();
 
 },1000/4); // Loops every 1/4 seconds.
+//Slow loops
+setInterval(function(){
+    send_item_merchant();
+    handleParty();
+},30000);
 
-// Learn Javascript: https://www.codecademy.com/learn/introduction-to-javascript
-// Write your own CODE: https://github.com/kaansoral/adventureland
-// NOTE: If the tab isn't focused, browsers slow down the game
-// NOTE: Use the performance_trick() function as a workaround
+function useSkills(target) {
+    var hp_multi = 8
+    //Taunts if not attacking warrior
+    if (can_use("taunt", target) && get_target_of(target) != character) {
+        use_skill("taunt", target);
+    }
+    //Stomps if target Max HP is higher (change logic to higher attack targets?)
+    if (can_use("stomp") && target.hp > target.max_hp * 0.50 && target.max_hp > character.attack * hp_multi) {
+        use_skill("stomp",target);
+    }
+    if (!is_on_cooldown("hardshell") && in_attack_range(target)) {
+        if(target.damage_type == 'physical' && target.attack > character.hp * 0.03 && get_target_of(target) == character) {
+        //Use hardshell -
+        use_skill('hardshell');
+        game_log('TANK MODE RAWR');
+        }
+    }
+}
+// 
+
+//Checks if skill is ready
+function useCharge(target) { 
+    if (can_use("charge", target)) {
+        game_log("Charge!");
+        use_skill("charge",target);
+    }
+}
